266309f45b3929f014b40b7f8f5aa1c778521755
Graeme McKerrell <31652935+gmckerrell@users.noreply.github.com>
2020-08-20 16:08:47 +0100

    Update Elide.java

--- elide-core/src/main/java/com/yahoo/elide/Elide.java
+++ elide-core/src/main/java/com/yahoo/elide/Elide.java
@@ -248,6 +248,21 @@ public class Elide {
     /**
      * Handle PATCH.
      *
+     * @param contentType the content type
+     * @param accept the accept
+     * @param path the path
+     * @param jsonApiDocument the json api document
+     * @param opaqueUser the opaque user
+     * @return Elide response object
+     */
+    public ElideResponse patch(String contentType, String accept,
+                               String path, String jsonApiDocument, Object opaqueUser) {
+        return patch(null, contentType, accept, path, jsonApiDocument, null, opaqueUser);
+    }
+
+    /**
+     * Handle PATCH.
+     *
      * @param baseUrlEndPoint base URL with prefix endpoint
      * @param contentType the content type
      * @param accept the accept
@@ -261,6 +276,23 @@ public class Elide {
         return patch(baseUrlEndPoint, contentType, accept, path, jsonApiDocument, null, opaqueUser);
     }
 
+    /**
+     * Handle PATCH.
+     *
+     * @param contentType the content type
+     * @param accept the accept
+     * @param path the path
+     * @param jsonApiDocument the json api document
+     * @param queryParams the query params
+     * @param opaqueUser the opaque user
+     * @return Elide response object
+     */
+    public ElideResponse patch(String baseUrlEndPoint, String contentType, String accept,
+                               String path, String jsonApiDocument,
+                               MultivaluedMap<String, String> queryParams, Object opaqueUser) {
+        return patch(null, contentType, accept, path, jsonApiDocument, queryParams, opaqueUser);
+    }
+    
     /**
      * Handle PATCH.
      *
@@ -305,6 +337,18 @@ public class Elide {
     /**
      * Handle DELETE.
      *
+     * @param path the path
+     * @param jsonApiDocument the json api document
+     * @param opaqueUser the opaque user
+     * @return Elide response object
+     */
+    public ElideResponse delete(String baseUrlEndPoint, String path, String jsonApiDocument, Object opaqueUser) {
+        return delete(null, path, jsonApiDocument, null, opaqueUser);
+    }
+
+    /**
+     * Handle DELETE.
+     *
      * @param baseUrlEndPoint base URL with prefix endpoint
      * @param path the path
      * @param jsonApiDocument the json api document
@@ -315,6 +359,20 @@ public class Elide {
         return delete(baseUrlEndPoint, path, jsonApiDocument, null, opaqueUser);
     }
 
+    /**
+     * Handle DELETE.
+     *
+     * @param path the path
+     * @param jsonApiDocument the json api document
+     * @param queryParams the query params
+     * @param opaqueUser the opaque user
+     * @return Elide response object
+     */
+    public ElideResponse delete(String path, String jsonApiDocument,
+                                MultivaluedMap<String, String> queryParams, Object opaqueUser) {
+        return delete(null, path, jsonApiDocument, queryParams, opaqueUser);
+    }
+    
     /**
      * Handle DELETE.
      *
